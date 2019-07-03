90c04972c85d5ca1c85028de4ac4b8bae079d0e5
lorenzo.brutti <lorenzo.brutti@metide.com>
2019-07-03 17:57:21 +0200

    upload

---
+++ background.js
@@ -0,0 +1,9 @@
+function logURL(requestDetails) {
+    console.log("***********");
+  console.log("Loading: " + requestDetails.url);
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  logURL,
+  {urls: ["<all_urls>"]}
+);
