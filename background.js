d2d77de562eb46cec55c4834599c7a58cce446cd
Lorenzo Brutti <lorenzobrutti80@gmail.com>
2019-07-04 01:12:05 +0200

    aggiunto script esterni

--- background.js
+++ background.js
@@ -1,6 +1,25 @@
 function logURL(requestDetails) {
     console.log("***********");
   console.log("Loading: " + requestDetails.url);
+  ciao();
+  var env = new Tone.AmplitudeEnvelope({
+			"attack" : 0.11,
+			"decay" : 0.21,
+			"sustain" : 0.5,
+			"release" : 1.2
+		}).toMaster();
+
+		//create an oscillator and connect it to the envelope
+		var osc = new Tone.Oscillator({
+			"partials" : [3, 2, 1],
+			"type" : "custom",
+			"frequency" : "C#4",
+			"volume" : -8,
+		}).connect(env).start();
+
+				env.triggerAttack();
+//				env.triggerRelease();
+
 }
 
 browser.webRequest.onBeforeRequest.addListener(
