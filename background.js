6eab021eb5d14b0af943f309e2cdc51f13b9d3c1
lorenzo.brutti <lorenzo.brutti@metide.com>
2019-07-04 09:44:14 +0200

    inserito altri handler

--- background.js
+++ background.js
@@ -1,28 +1,35 @@
-function logURL(requestDetails) {
-    console.log("***********");
-  console.log("Loading: " + requestDetails.url);
-  ciao();
-  var env = new Tone.AmplitudeEnvelope({
-			"attack" : 0.11,
-			"decay" : 0.21,
-			"sustain" : 0.5,
-			"release" : 1.2
-		}).toMaster();
+function onBeforeRequestHandler(requestDetails) {
+	console.log("***********");
+	console.log("Loading: " + requestDetails.url);
 
-		//create an oscillator and connect it to the envelope
-		var osc = new Tone.Oscillator({
-			"partials" : [3, 2, 1],
-			"type" : "custom",
-			"frequency" : "C#4",
-			"volume" : -8,
-		}).connect(env).start();
+	oscSet[requestDetails.url] = oscSet[requestDetails.url] || new Tone.Oscillator({
+		"partials": [3, 2, 1],
+		"type": "custom",
+		"frequency": "C#4",
+		"volume": -8,
+	}).connect(env).start();
 
-				env.triggerAttack();
-//				env.triggerRelease();
+	env.triggerAttack();
+	//				env.triggerRelease();
 
 }
 
+function onCompleteddHandler(requestDetails){
+	env.triggerRelease();
+}
+let env = new Tone.AmplitudeEnvelope({
+	"attack": 0.11,
+	"decay": 0.21,
+	"sustain": 0.5,
+	"release": 1.2
+}).toMaster();
+let oscSet = {};
 browser.webRequest.onBeforeRequest.addListener(
-  logURL,
-  {urls: ["<all_urls>"]}
+	logURL,
+	{ urls: ["<all_urls>"] }
+);
+
+browser.webRequest.onCompleted.addListener(
+	onCompleteddHandler, 
+	{ urls: ["<all_urls>"] }
 );
