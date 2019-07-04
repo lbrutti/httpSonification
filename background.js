4fa8ee4e452f44137b6300b7917f101705e19d77
lorenzo.brutti <lorenzo.brutti@metide.com>
2019-07-04 17:53:11 +0200

    modifiche

--- background.js
+++ background.js
@@ -1,34 +1,42 @@
-function onBeforeRequestHandler(requestDetails) {
-	console.log("***********");
-	console.log("Loading: " + requestDetails.url);
 
-	oscSet[requestDetails.url] = oscSet[requestDetails.url] || new Tone.Oscillator({
-		"partials": [3, 2, 1],
-		"type": "custom",
-		"frequency": "C#4",
-		"volume": -8,
-	}).connect(env).start();
-
-	env.triggerAttack();
-
-}
-
-function onCompleteddHandler(requestDetails){
-	env.triggerRelease();
-}
 let env = new Tone.AmplitudeEnvelope({
 	"attack": 0.11,
 	"decay": 0.21,
 	"sustain": 0.5,
 	"release": 1.2
 }).toMaster();
+
+let envSet = {};
 let oscSet = {};
+
+function onBeforeRequestHandler(requestDetails) {
+	envSet[requestDetails.url]=envSet[requestDetails.url] || new Tone.AmplitudeEnvelope({
+		"attack": 0.5,
+		"decay": 0,
+		"sustain": 0,
+		"release": 0
+	}).toMaster();
+	oscSet[requestDetails.url] = oscSet[requestDetails.url] || new Tone.Oscillator({
+		"partials": [3, 2, 1],
+		"type": "custom",
+		"frequency": Math.random()*20000,
+		"volume": -8,
+	}).connect(envSet[requestDetails.url]).start();
+
+	envSet[requestDetails.url].triggerAttack();
+
+}
+
+function onCompletedHandler(requestDetails){
+	envSet[requestDetails.url].triggerRelease();
+}
+
 browser.webRequest.onBeforeRequest.addListener(
 	onBeforeRequestHandler,
 	{ urls: ["<all_urls>"] }
 );
 
 browser.webRequest.onCompleted.addListener(
-	onCompleteddHandler, 
+	onCompletedHandler, 
 	{ urls: ["<all_urls>"] }
 );
